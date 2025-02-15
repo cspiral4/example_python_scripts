e2dd69b008b6527a71abac1b69398c8a97e736ed
Beth Jelich <spiral4@comcast.net>
2025-02-14 22:36:44 -0500

    script cleanup

--- selenium_testing/chrome_test.py
+++ selenium_testing/chrome_test.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-import time
 
 # Set up ChromeDriver
 chrome_driver_path = "C:/Users/beth/bin/chromedriver.exe"
